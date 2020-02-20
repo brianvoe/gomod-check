87d781869b75075f63e20f86e5639f0ee3c8d801
brianvoe <brian@webiswhatido.com>
2020-02-19 23:35:25 -0600

    proxy request - added get requests

--- main.go
+++ main.go
@@ -3,8 +3,9 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
 
-	"github.com/brianvoe/gofakeit/v4"
+	"github.com/brianvoe/gofakeit"
 	"golang.org/x/mod/modfile"
 )
 
@@ -34,6 +35,23 @@ func main() {
 		}
 
 		fmt.Println(r.Mod.Path, " ", r.Mod.Version)
+		getVersions(r.Mod.Path)
 	}
 
+}
+
+func getVersions(url string) {
+	resp, err := http.Get(fmt.Sprintf("https://proxy.golang.org/%s/@v/list", url))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(body))
 }
